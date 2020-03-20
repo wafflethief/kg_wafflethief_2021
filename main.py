b4e9ac08ebb0757a04bef13743d9dc6b76f8f601
Janis Louie <jlouie2@binghamton.edu>
2020-03-20 15:06:15 -0400

    Changed code to count unique chars

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 from testMapping import isOneToOne
+
+MAX_CHARS = 256
+def areIsomorphic(string1, string2):
+    m = len(string1)
+    n = len(string2)
+    if m != n:
+        return False
+    marked = [False] * MAX_CHARS
+    map = [-1] * MAX_CHARS
+    for i in range(n):
+        if map[ord(string1[i])] == -1:
+            if marked[ord(string2[i])] == True:
+                return False
+            marked[ord(string2[i])] = True
+            map[ord(string1[i])] = string2[i]
+        elif map[ord(string1[i])] != string2[i]:
+            return False
+    return True
+
 def main():
+    """print(areIsomorphic("abb", "aba"))
+    print(areIsomorphic("aab", "foo")) # True
+    print(areIsomorphic("bar", "foo")) # True
+    print(areIsomorphic("foo", "bar")) # False
+    print(areIsomorphic("fff", "bar")) # False
+    print(areIsomorphic("f", "bar")) # False
+    """
     print(isOneToOne("abb", "aba")) # True
     print(isOneToOne("aab", "foo")) # True
     print(isOneToOne("bar", "foo")) # True
     print(isOneToOne("foo", "bar")) # False
-    print(isOneToOne("foo", "bar")) # False
-    print(isOneToOne("foo", "bar")) # False
+    print(isOneToOne("fff", "bar")) # False
+    print(isOneToOne("f", "bar")) # False
 main()
--- testMapping.py
+++ testMapping.py
@@ -2,18 +2,32 @@ def isOneToOne(s1, s2):
     if len(s1) is not len(s2):
         return False
     mappings = {}
+    #abb, aba
     #for i in range(len(s1)): #iterate through first str
     #    if mappings[s1[i]] is None:
     s2_used = [-1]*256
-    for i in range(len(s1)):
-        if mappings.get(s1[i]) is None: #current char not in mapping
-            if s2_used[ord(s1[i])] is -1: # mapping available
-                mappings[s1[i]] = s2[i]
-                s2_used[ord(s1[i])] = s2[i]
-            else: # mapping unavailable
+    unique_s1 = [];
+    unique_s2 = [];
+    """for i in range(len(s1)):
+        #if mappings.get(s1[i]) is None: #current char not in mapping
+        if s2_used[ord(s1[i])] is -1: # mapping available
+            mappings[s1[i]] = s2[i]
+            s2_used[ord(s1[i])] = s2[i]
+        else: # mapping unavailable
                 return False
-        else: # found a mapping
-            if mappings.get(s1[i]) is not s2_used[ord(s1[i])]:
-                #if a mapping in s1 is mapped to two places
-                return False
-    return True
+        if mappings.get(s1[i]) is not s2_used[ord(s1[i])]:
+            #if a mapping in s1 is mapped to two places
+            print("pottery")
+            return False """
+    for c in s1[::]:
+        if c not in unique_s1:
+            unique_s1.append(c)
+    for c in s2[::]:
+        if c not in unique_s2:
+            unique_s2.append(c)
+    print(len(unique_s1))
+    print(len(unique_s2))
+    if len(unique_s1) < len(unique_s2):
+        return False;
+    return True;
+    #return True
